72fa4c615d76663dfa0b447a09b720bb06f53e30
Maimuna Morshed <maimuna.morshed@g.bracu.ac.bd>
2025-12-27 01:43:59 +0600

    Delete frontend directory

--- frontend/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: 40 }}>
-      <h1>Welcome to ReadTrack 📚</h1>
-      <p>Track your reading journey and discover amazing books.</p>
-    </div>
-  );
-}
